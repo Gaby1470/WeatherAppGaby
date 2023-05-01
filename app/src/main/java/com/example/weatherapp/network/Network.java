5c10c257bd71d2e1eedbb12d2c3b9249badf47b8
gs0522 <gs0522@dexcom.com>
2023-05-01 09:56:21 -0700

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/network/Network.java
@@ -0,0 +1,10 @@
+package com.example.weatherapp.network;
+
+public class Network {
+
+    public static String openWeatherAPI = "http://api.weatherapi.com/v1/";
+
+//    public String openWeatherAPIKey = "12adb782312740b0af2221557232603"; // api profe
+    public String openWeatherAPIKey = "6a82ed76d3cb4828aaa32240232504"; // mi api
+
+}
